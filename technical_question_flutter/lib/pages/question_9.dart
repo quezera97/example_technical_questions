51b77ab0777c239adbf2c08fef309393902d7323
LoleeconDestroyer <66286710+quezera97@users.noreply.github.com>
2023-12-14 10:59:18 +0800

    question 9 - flutter

--- technical_question_flutter/lib/pages/question_9.dart
+++ technical_question_flutter/lib/pages/question_9.dart
@@ -1,5 +1,9 @@
 // String Manipulation: Create a function that takes in a string and returns the reverse of that string
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 import '../widget/reuse_widget.dart';
 
@@ -12,6 +16,37 @@ class Question9 extends StatefulWidget {
 
 class _Question9State extends State<Question9> {
   final generateWidget = GenerateWidget();
+  final numberController = TextEditingController(text: '5');
+  List<int> arrDigit = [];
+  int largestInt = 0;
+  Random random = Random();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    generateAndFindNumber();
+  }
+
+  void generateAndFindNumber() {
+    for (int i = 0; i < int.parse(numberController.text); i++) {
+      int randomNumber = random.nextInt(100);
+      arrDigit.add(randomNumber);
+    }
+
+    largestInt = arrDigit[0];
+
+    for(int i = 1; i < int.parse(numberController.text); i++) {
+      if(arrDigit[i] > largestInt){
+        largestInt = arrDigit[i];
+      }
+    }
+
+    setState(() {
+      arrDigit;
+      largestInt;
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -25,7 +60,32 @@ class _Question9State extends State<Question9> {
           padding: const EdgeInsets.all(10.5),
           child: Column(
             children: [
-            ]
+              const Padding(
+                padding: EdgeInsets.all(15),
+                child: HtmlWidget(
+                  '''
+                    <h3> Write an algorithm that finds the largest number in an array of integers without using built-in functions like max() </h3>
+                  ''',
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              generateWidget.createTextFormField(numberController, '', 'Enter size of array', (p0) => null, [FilteringTextInputFormatter.allow(RegExp("[0-9]"))], TextInputType.number),
+            
+              generateWidget.createSimpleButton('Generate number', () {
+                arrDigit = [];
+
+                generateAndFindNumber();
+              }),
+              const SizedBox(height: 50),
+              const Text('Generated list of array:', style: TextStyle(fontSize: 15)),
+              const SizedBox(height: 10),
+              Text(arrDigit.toString(), style: const TextStyle(fontSize: 25)),
+              const SizedBox(height: 30),
+              const Text('The largest number in the array is:', style: TextStyle(fontSize: 15)),
+              const SizedBox(height: 10),
+              Text(largestInt.toString(), style: const TextStyle(fontSize: 25)),
+            ],
           ),
         ),
       ),
